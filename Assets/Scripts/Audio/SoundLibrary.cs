91fbd428c7170a6e5be61fe847a90c6bdb22d9dc
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-11-03 00:19:53 -0600

    Sound library class.

--- Assets/Scripts/Audio/SoundLibrary.cs
+++ Assets/Scripts/Audio/SoundLibrary.cs
@@ -12,7 +12,13 @@ public class SoundLibrary : ScriptableObject {
 
     public AudioClip GetClip(string name, int idx = -1) {
         foreach (SoundEntry entry in entries) {
-
+            if (entry.name.Equals(name)) {
+                // Get 
+                if (idx < 0 || idx > entry.clips.Length) {
+                    idx = UnityEngine.Random.Range(0, entry.clips.Length);
+                }
+                return entry.clips[idx];
+            }
         }
         return null;
     }
