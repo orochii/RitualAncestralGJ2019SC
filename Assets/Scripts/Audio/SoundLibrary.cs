3ac95234d177413708bce7a1105ec781c4ab0ac6
Orlando Marin Blanco <arcboundcrusher@gmail.com>
2019-11-03 00:16:56 -0600

    Changes to tiles, hopefully the file gets restores.

---
+++ Assets/Scripts/Audio/SoundLibrary.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundLibrary : ScriptableObject {
+    public class SoundEntry {
+        public string name;
+        public AudioClip[] clips;
+    }
+
+    SoundEntry[] entries;
+
+    public AudioClip GetClip(string name, int idx = -1) {
+        foreach (SoundEntry entry in entries) {
+
+        }
+        return null;
+    }
+}
