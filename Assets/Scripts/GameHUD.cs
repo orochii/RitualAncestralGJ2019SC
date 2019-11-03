e2d48fb1c4885f86c0198bff25f5f28562e16368
Bryan Hernandez <hernandezbryan2893gmail.com>
2019-11-03 12:47:39 -0600

    Preliminary version.

--- Assets/Scripts/GameHUD.cs
+++ Assets/Scripts/GameHUD.cs
@@ -5,8 +5,12 @@ using UnityEngine.UI;
 
 public class GameHUD : MonoBehaviour {
     [SerializeField] private Text coinsText;
+    [SerializeField] private Image[] flagVisual;
 
     private void FixedUpdate() {
         coinsText.text = GameManager.Coins.ToString();
+        for (int i = 0; i < flagVisual.Length; i++) {
+            flagVisual[i].gameObject.SetActive(GameManager.GetFlag(i));
+        } 
     }
 }
--- Assets/Scripts/Scenario/FlagMarker.cs
+++ Assets/Scripts/Scenario/FlagMarker.cs
@@ -7,12 +7,16 @@ public class FlagMarker : MonoBehaviour
     [SerializeField] private int idx;
     [SerializeField] private bool val;
     [SerializeField] private GameObject activate;
+    [SerializeField] private ParticleSystem collectEffect;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         if (collision.CompareTag("Player")) {
             if (activate == null) activate.SetActive(true);
             GameManager.SetFlag(idx, val);
             gameObject.SetActive(false);
+            ParticleSystem fx = Instantiate<ParticleSystem>(collectEffect, transform.position, Quaternion.identity);
+            Destroy(fx.gameObject, 1f);
+            AudioManager.instance.PlaySound("coins", transform.position);
         }
     }
 }
